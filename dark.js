dcedf09c6835dd71f534d4396efc47d9b1fd4665
innacp <inna.chapkevych@gmail.com>
2023-01-20 15:37:36 +0200

    fix images

--- dark.js
+++ dark.js
@@ -3,6 +3,15 @@ const button = document.querySelector(".dark-btn")
 
 button.addEventListener('click', () => {
     body.classList.toggle('dark-mode')
+    changeBtnText()
 })
 
+function changeBtnText() {
+    const hasBodyDarkMode = body.classList.contains('dark-mode')
+    if( hasBodyDarkMode ) {
+        button.innerHTML = 'To Light Theme'
+    } else {
+        button.innerHTML = 'To Dark Theme'
+    }
+}
 
--- dist/dark.dev.js
+++ dist/dark.dev.js
@@ -4,4 +4,15 @@ var body = document.querySelector("body");
 var button = document.querySelector(".dark-btn");
 button.addEventListener('click', function () {
   body.classList.toggle('dark-mode');
+  changeBtnText();
 });
+
+function changeBtnText() {
+  var hasBodyDarkMode = body.classList.contains('dark-mode');
+
+  if (hasBodyDarkMode) {
+    button.innerHTML = 'To Light Theme';
+  } else {
+    button.innerHTML = 'To Dark Theme';
+  }
+}
